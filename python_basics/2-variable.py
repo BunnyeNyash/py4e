821c55bd92fd3a768f608b7fc4ac06799fbe9924
BunnyeNyash <otachibosibori@gmail.com>
2023-07-02 22:55:32 +0300

    variables

---
+++ python_basics/2-variable.py
@@ -0,0 +1,14 @@
+#a name given to a value
+
+#Examples
+n = 5
+pi = 3.141593
+my_Age = 32
+my_Favorite_toy = "Piggy"
+
+#The assignment statement creates the variables and then gives them the values
+print(n)
+print(pi)
+print(my_Age)
+print(my_Favorite_toy)
+
