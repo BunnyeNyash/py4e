862bd132ee20658eab7802ddfafd70b537357d3f
BunnyeNyash <otachibosibori@gmail.com>
2023-07-03 12:59:33 +0300

    rm

--- python_basics/gross_pay.py
+++
@@ -1,9 +0,0 @@
-# Prompts the user for working hours
-hours = float(input("What is the total number of your working hours? "))
-
-# Prompts the user for pay rate per hour
-rate = float(input("What is your pay rate on a hourly basis? "))
-
-gross_pay = hours * rate
-
-print("Your gross pay is: ", gross_pay)
