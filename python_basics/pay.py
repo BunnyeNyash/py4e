9ce03350487e32af7665b6830a402f20ca154ca5
BunnyeNyash <otachibosibori@gmail.com>
2023-07-03 12:54:33 +0300

    Python Program that prompts the user for their working hour, pay rate per hour, and then computes the gross pay

---
+++ python_basics/pay.py
@@ -0,0 +1,11 @@
+# Prompts the user for working hours
+hours = input("What is the total number of your working hours? "))
+
+# Prompts the user for pay rate per hour
+rate = input("What is your pay rate on a hourly basis? "))
+
+int(gross_pay)
+
+gross_pay = hours * rate
+
+print("Your gross pay is: ", gross_pay)
