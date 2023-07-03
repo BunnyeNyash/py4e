afb9984fa7b962691e1c35f6dcecc612e7717bc4
BunnyeNyash <otachibosibori@gmail.com>
2023-07-03 14:05:17 +0300

    program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature

---
+++ temperature_conversion.py
@@ -0,0 +1,9 @@
+# Prompts the user for a Celsius temperature
+celsius = input("Enter the temperature in celsius degrees: ")
+
+celsius = float(celsius)
+
+fahrenheit = (celsius * 9 / 5) + 32
+
+# Prints the temperature converted to fahrenheit
+print("The temperature in Farenheit is: ", fahrenheit)
