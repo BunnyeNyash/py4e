0d5b84cae35f18b2be8867539ba13271df728a37
BunnyeNyash <otachibosibori@gmail.com>
2023-07-03 13:03:48 +0300

    rm

--- python_basics/user_welcome.py
+++
@@ -1,6 +0,0 @@
-# prompt asking for the user's name
-name = input("What is your name?\n")
-
-# print the welcome message
-print("Welcome, " + name + "!")
-
--- python_basics/welcome.py
+++
@@ -1,5 +0,0 @@
-# Prompt the user for their name
-name = input("What is your name? ")
-
-# Print the welcome message
-print("Welcome, ", name, "!")
