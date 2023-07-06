4e58e40a79c4126b2391f563d5b0f5147f167695
BunnyeNyash <otachibosibori@gmail.com>
2023-07-06 15:24:14 +0300

    program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error message. If the score is between 0.0 and 1.0, print a grade ( A >= 0.9, B >= 0.8, C >= 0. 7, D >= 0.6, F < 0.6)

---
+++ score2.py
@@ -0,0 +1,22 @@
+# Prompts user for a score
+score = input("Score between 0.0 and 1.0: ")
+
+try :
+    score = float(score)
+
+    if 0.0 <= score <= 1.0 :
+        if score >= 0.9 :
+            print("A")
+        elif score >= 0.8 :
+            print("B")
+        elif score >= 0.7 :
+            print("C")
+        elif score >= 0.6 :
+            print("D")
+        elif score < 0.6 :
+            print("F")
+    else :
+        print("Bad Score")
+
+except ValueError : 
+    print("Bad Score")
