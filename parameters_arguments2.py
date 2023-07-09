13d6f6a8b74651a6878b67085a13a3a6d1e28c59
BunnyeNyash <otachibosibori@gmail.com>
2023-07-09 16:24:22 +0300

    one parameter, multiple arguments

---
+++ parameters_arguments2.py
@@ -0,0 +1,9 @@
+def print_twice(bruce):
+    print(bruce)
+    print(bruce)
+
+toy_car_name = "Speedy"
+print_twice(toy_car_name)
+
+another_toy_car_name = "Racer"
+print_twice(another_toy_car_name)
